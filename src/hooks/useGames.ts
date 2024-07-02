a25d943c8b6214a66c8d7644d91e94dceccf7a27
Bisma Ijaz <bismaijaz@gosaas.io>
2024-07-02 12:47:27 +0500

    building game cards

---
+++ src/hooks/useGames.ts
@@ -0,0 +1,31 @@
+import { CanceledError } from "axios";
+import apiClient from "../services/api-client";
+import { useState,useEffect } from "react";
+export interface Game {
+    id: number;
+    name: string;
+    background_image:string
+  }
+  interface FetchGameResponse {
+    count: number;
+    results: Game[];
+  }
+
+const  useGames =()=>{
+    const [games, setGames] = useState<Game[]>([]);
+  const [error, setError] = useState("");
+  useEffect(() => {
+      const controller = new AbortController();
+      apiClient
+      .get<FetchGameResponse>("/games",{signal: controller.signal})
+      .then((res) => setGames(res.data.results))
+      .catch((err) => {
+        if(err instanceof CanceledError) 
+            return;
+        setError(err.message)});
+    return () => controller.abort();
+  },[]);
+  return {games, error}
+
+}
+export default useGames;
